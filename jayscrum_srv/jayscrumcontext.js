c96fe724f23e35a0d0967f6c6e0a0b83c06eb5d2
Peter Nochta <peter.nochta@jaydata.org>
2012-07-18 17:49:21 +0200

    add jayscrum server

---
+++ jayscrum_srv/jayscrumcontext.js
@@ -0,0 +1,158 @@
+(function (global, $data, undefined) {
+    function registerEdmTypes() {
+
+        function Edm_Boolean() { };
+        $data.Container.registerType('Edm.Boolean', Edm_Boolean);
+        $data.Container.mapType(Edm_Boolean, $data.Boolean);
+
+        function Edm_Binary() { };
+        $data.Container.registerType('Edm.Binary', Edm_Binary);
+        $data.Container.mapType(Edm_Binary, $data.Blob);
+
+        function Edm_DateTime() { };
+        $data.Container.registerType('Edm.DateTime', Edm_DateTime);
+        $data.Container.mapType(Edm_DateTime, $data.Date);
+
+        function Edm_DateTimeOffset() { };
+        $data.Container.registerType('Edm.DateTimeOffset', Edm_DateTimeOffset);
+        $data.Container.mapType(Edm_DateTimeOffset, $data.Integer);
+
+        function Edm_Time() { };
+        $data.Container.registerType('Edm.Time', Edm_Time);
+        $data.Container.mapType(Edm_Time, $data.Integer);
+
+        function Edm_Decimal() { };
+        $data.Container.registerType('Edm.Decimal', Edm_Decimal);
+        $data.Container.mapType(Edm_Decimal, $data.Number);
+
+        function Edm_Single() { };
+        $data.Container.registerType('Edm.Single', Edm_Single);
+        $data.Container.mapType(Edm_Single, $data.Number);
+
+        function Edm_Double() { };
+        $data.Container.registerType('Edm.Double', Edm_Double);
+        $data.Container.mapType(Edm_Double, $data.Number);
+
+        function Edm_Guid() { };
+        $data.Container.registerType('Edm.Guid', Edm_Guid);
+        $data.Container.mapType(Edm_Guid, $data.String);
+
+        function Edm_Int16() { };
+        $data.Container.registerType('Edm.Int16', Edm_Int16);
+        $data.Container.mapType(Edm_Int16, $data.Integer);
+
+        function Edm_Int32() { };
+        $data.Container.registerType('Edm.Int32', Edm_Int32);
+        $data.Container.mapType(Edm_Int32, $data.Integer);
+
+        function Edm_Int64() { };
+        $data.Container.registerType('Edm.Int64', Edm_Int64);
+        $data.Container.mapType(Edm_Int64, $data.Integer);
+
+        function Edm_Byte() { };
+        $data.Container.registerType('Edm.Byte', Edm_Byte);
+        $data.Container.mapType(Edm_Byte, $data.Integer);
+
+        function Edm_String() { };
+        $data.Container.registerType('Edm.String', Edm_String);
+        $data.Container.mapType(Edm_String, $data.String);
+
+    };
+    registerEdmTypes();
+    $data.Entity.extend('LightSwitchApplication.WorkItem', {
+        'Id': { key: true, type: 'Edm.String', nullable: false, computed: true },
+        'RowVersion': { type: 'Edm.Binary', nullable: false, concurrencyMode: $data.ConcurrencyMode.Fixed, computed: true },
+        'Title': { type: 'Edm.String', nullable: false, required: true, maxLength: 255 },
+        'Type': { type: 'Edm.String', nullable: false, required: true, maxLength: 255 },
+        'Description': { type: 'Edm.String', maxLength: 255 },
+        'CreatedDate': { type: 'Edm.DateTime', nullable: false, required: true },
+        'CreatedBy': { type: 'Edm.String', nullable: false, required: true, maxLength: 255 },
+        'ChangedDate': { type: 'Edm.DateTime', nullable: false, required: true },
+        'ChangedBy': { type: 'Edm.String', nullable: false, required: true, maxLength: 255 },
+        'Priority': { type: 'Edm.Int32' },
+        'AssignedTo': { type: 'Edm.String', maxLength: 255 },
+        'ParentName': { type: 'Edm.String', maxLength: 255 },
+        'ProjectName': { type: 'Edm.String', maxLength: 255 },
+        'State': { type: 'Edm.String', maxLength: 255 },
+        'Effort': { type: 'Edm.Int32' },
+        'BusinessValue': { type: 'Edm.Int32' },
+        'IsBlocked': { type: 'Edm.Boolean' },
+        'RemainingWork': { type: 'Edm.Int32' },
+        'WorkItem_Project': { type: 'Edm.Int32' },
+        'WorkItem_Sprint': { type: 'Edm.Int32' },
+        'WorkItem_WorkItem': { type: 'Edm.Int32' },
+        'Reason': { type: 'Edm.String', maxLength: 255 }
+        //'Project': { type: 'LightSwitchApplication.Project', inverseProperty: 'WorkItems' },
+        //'Sprint': { type: 'LightSwitchApplication.Sprint', inverseProperty: 'WorkItems' },
+        //'Parent': { type: 'LightSwitchApplication.WorkItem', inverseProperty: 'Children' },
+        //'Children': { type: 'Array', elementType: 'LightSwitchApplication.WorkItem', inverseProperty: 'Parent' }
+    });
+    $data.Entity.extend('LightSwitchApplication.Project', {
+        'Id': { key: true, type: 'Edm.String', nullable: false, computed: true },
+        'RowVersion': { type: 'Edm.Binary', nullable: false, concurrencyMode: $data.ConcurrencyMode.Fixed, computed: true },
+        'Name': { type: 'Edm.String', nullable: false, required: true, maxLength: 255 },
+        'Description': { type: 'Edm.String', maxLength: 255 }
+        //'WorkItems': { type: 'Array', elementType: 'LightSwitchApplication.WorkItem', inverseProperty: 'Project' }
+    });
+    $data.Entity.extend('LightSwitchApplication.Sprint', {
+        'Id': { key: true, type: 'Edm.String', nullable: false, computed: true },
+        'RowVersion': { type: 'Edm.Binary', nullable: false, concurrencyMode: $data.ConcurrencyMode.Fixed, computed: true },
+        'Name': { type: 'Edm.String', nullable: false, required: true, maxLength: 255 },
+        'StartDate': { type: 'Edm.DateTime', nullable: false, required: true },
+        'FinishDate': { type: 'Edm.DateTime', nullable: false, required: true }
+        //'WorkItems': { type: 'Array', elementType: 'LightSwitchApplication.WorkItem', inverseProperty: 'Sprint' }
+    });
+    $data.EntityContext.extend('LightSwitchApplication.ApplicationData', {
+        WorkItems: { type: $data.EntitySet, elementType: LightSwitchApplication.WorkItem },
+        Projects: { type: $data.EntitySet, elementType: LightSwitchApplication.Project },
+        Sprints: { type: $data.EntitySet, elementType: LightSwitchApplication.Sprint }/*,
+        Microsoft_LightSwitch_GetCanInformation: $data.EntityContext.generateServiceOperation({ serviceName: 'Microsoft_LightSwitch_GetCanInformation', returnType: 'Edm.String', params: [{ dataServiceMembers: 'Edm.String' }], method: 'GET' })*/
+    });
+    ///Security
+    /*$data.Entity.extend('Microsoft.LightSwitch.Security.UserRegistration', {
+        'UserName': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 256 },
+        'FullName': { type: 'Edm.String', nullable: false, required: true, maxLength: 256, concurrencyMode: $data.ConcurrencyMode.Fixed },
+        'Password': { type: 'Edm.String', maxLength: 128, concurrencyMode: $data.ConcurrencyMode.Fixed },
+        'RoleAssignments': { type: 'Array', elementType: 'Microsoft.LightSwitch.Security.RoleAssignment', inverseProperty: 'User' },
+        'DerivingRoleAssignments': { type: 'Array', elementType: 'Microsoft.LightSwitch.Security.RoleAssignment', inverseProperty: 'SourceAccount' }
+    });
+    $data.Entity.extend('Microsoft.LightSwitch.Security.Role', {
+        'Name': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 128 },
+        'RoleAssignments': { type: 'Array', elementType: 'Microsoft.LightSwitch.Security.RoleAssignment', inverseProperty: 'Role' },
+        'RolePermissions': { type: 'Array', elementType: 'Microsoft.LightSwitch.Security.RolePermission', inverseProperty: 'Role' }
+    });
+    $data.Entity.extend('Microsoft.LightSwitch.Security.RoleAssignment', {
+        'UserName': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 256 },
+        'RoleName': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 128 },
+        'SourceAccountName': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 256 },
+        'User': { type: 'Microsoft.LightSwitch.Security.UserRegistration', required: true, inverseProperty: 'RoleAssignments' },
+        'SourceAccount': { type: 'Microsoft.LightSwitch.Security.UserRegistration', required: true, inverseProperty: 'DerivingRoleAssignments' },
+        'Role': { type: 'Microsoft.LightSwitch.Security.Role', required: true, inverseProperty: 'RoleAssignments' }
+    });
+    $data.Entity.extend('Microsoft.LightSwitch.Security.Permission', {
+        'Id': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 322 },
+        'Name': { type: 'Edm.String', nullable: false, required: true, maxLength: 64, concurrencyMode: $data.ConcurrencyMode.Fixed },
+        'RolePermissions': { type: 'Array', elementType: 'Microsoft.LightSwitch.Security.RolePermission', inverseProperty: 'Permission' }
+    });
+    $data.Entity.extend('Microsoft.LightSwitch.Security.RolePermission', {
+        'RoleName': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 128 },
+        'PermissionId': { key: true, type: 'Edm.String', nullable: false, required: true, maxLength: 322 },
+        'Role': { type: 'Microsoft.LightSwitch.Security.Role', required: true, inverseProperty: 'RolePermissions' },
+        'Permission': { type: 'Microsoft.LightSwitch.Security.Permission', required: true, inverseProperty: 'RolePermissions' }
+    });
+    $data.EntityContext.extend('Microsoft.LightSwitch.Security.SecurityData', {
+        UserRegistrations: { type: $data.EntitySet, elementType: Microsoft.LightSwitch.Security.UserRegistration },
+        Roles: { type: $data.EntitySet, elementType: Microsoft.LightSwitch.Security.Role },
+        RoleAssignments: { type: $data.EntitySet, elementType: Microsoft.LightSwitch.Security.RoleAssignment },
+        Permissions: { type: $data.EntitySet, elementType: Microsoft.LightSwitch.Security.Permission },
+        RolePermissions: { type: $data.EntitySet, elementType: Microsoft.LightSwitch.Security.RolePermission },
+        Microsoft_LightSwitch_GetCanInformation: $data.EntityContext.generateServiceOperation({ serviceName: 'Microsoft_LightSwitch_GetCanInformation', returnType: 'Edm.String', params: [{ dataServiceMembers: 'Edm.String' }], method: 'GET' }),
+        ChangePassword: $data.EntityContext.generateServiceOperation({ serviceName: 'ChangePassword', returnType: null, params: [{ userName: 'Edm.String' }, { oldPassword: 'Edm.String' }, { newPassword: 'Edm.String' }], method: 'POST' }),
+        IsValidPassword: $data.EntityContext.generateServiceOperation({ serviceName: 'IsValidPassword', returnType: 'Edm.Boolean', params: [{ password: 'Edm.String' }], method: 'POST' }),
+        GetWindowsUserInfo: $data.EntityContext.generateServiceOperation({ serviceName: 'GetWindowsUserInfo', returnType: 'Edm.String', params: [{ userName: 'Edm.String' }], method: 'GET' })
+    });*/
+
+    //Microsoft.LightSwitch.Security.context = new Microsoft.LightSwitch.Security.SecurityData({ name: 'oData', oDataServiceHost: 'http://localhost/JayScrumServer_ls/Microsoft.LightSwitch.SecurityData.svc' });
+
+
+})(window, $data);
---
+++ jayscrum_srv/jayscrumserver.js
@@ -0,0 +1,128 @@
+require('mongodb');
+var $data = require('jaydata');
+var ctx = require('./jayscrumcontext.js');
+var $test = {};
+var connect = require('connect');
+var app = require('connect')();
+
+app.use(connect.multipart());
+app.use(connect.urlencoded());
+app.use(connect.json());
+app.use(connect.bodyParser());
+app.use(connect.query());
+
+
+app.use('/', function(req, res){
+    console.log('');console.log('');console.log('');
+    console.log('Request start');
+	if(req.url === "/favicon.ico"){
+		console.log("get favicon");
+		res.writeHead(401,{});
+        res.end();
+	}else{
+	    console.log(JSON.stringify(req.url));
+	    if (req.method === 'GET'){
+		$test.context = new LightSwitchApplication.ApplicationData({ name: 'mongoDB', databaseName: 'jayScrum' });
+		$test.context.onReady(function(db){
+		    var callback = function(result){
+		        res.write(JSON.stringify(result));
+			res.writeHead(200, {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
+				'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+			});
+		        res.end();
+		    };
+		    
+		    var compiled = req.query.expression || {};
+		    
+		    var qs = db[req.query.entitySet] || db;
+		    if (compiled.$serviceOperation){
+		        var fn = qs[compiled.$serviceOperation.name];
+		        if ((typeof fn.returnType.isAssignableTo === 'function' && fn.returnType.isAssignableTo($data.Queryable))){
+		            qs = fn.apply(this, compiled.$serviceOperation.params);
+		        }else{
+		            callback(fn.apply(this, compiled.$serviceOperation.params));
+		        }
+		    }else{
+		        /*if (compiled.$include){
+		            for (var i = 0; i < compiled.$include.length; i++){
+		                qs += '.include("' + compiled.$include[i] + '")';
+		            }
+		        }*/
+	console.log("//////////");
+	console.log(JSON.stringify(compiled));
+	console.log("//////////");
+		        if (compiled.$filter) qs = qs.filter(compiled.$filter);
+		        if (compiled.$order){
+		            /*for (var i = 0; i < compiled.$order.length; i++){
+		console.log(JSON.stringify(compiled.$order));
+		console.log(compiled.$order[i].fn);
+		                qs = qs[(JSON.parse(compiled.$order[i].direction) ? 'orderBy' : 'orderByDescending')](compiled.$order[i]);
+		            }*/
+				 qs = qs[JSON.parse(compiled.$order[1]) ? 'orderBy' : 'orderByDescending'](compiled.$order[0]);
+		        }
+		        if (compiled.$take) qs = qs.take(compiled.$take);
+		        if (compiled.$skip) qs = qs.skip(compiled.$skip);
+		        if (compiled.$length) qs.length(callback);
+		        else qs.toArray(callback);
+		    }
+		});
+	    }else if (req.method === 'POST'){
+		$test.context = new LightSwitchApplication.ApplicationData({ name: 'mongoDB', databaseName: 'jayScrum' });
+		$test.context.onReady(function(db){
+		    var callback = $data.typeSystem.createCallbackSetting({
+		        success: function(cnt){
+			    res.writeHead(200, {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
+				'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+			    });
+		            res.write(cnt.toString());
+		            res.end();
+		        },
+		        error: function(err){
+		            res.writeHead(200, {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
+				'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+			    });
+			    res.write('ohoh');
+		            res.end();
+		        }
+		    });
+		    
+		    var collections = JSON.parse(req.body.items);
+		    
+		    for (var es in collections){
+		        var c = collections[es];
+		        if (c.insertAll && c.insertAll.length){
+		            for (var i = 0; i < c.insertAll.length; i++)
+		                db[es].add(c.insertAll[i]);
+		        }
+		        
+		        if (c.removeAll && c.removeAll.length){
+		            for (var i = 0; i < c.removeAll.length; i++)
+		                db[es].remove(c.removeAll[i]);
+		        }
+		        
+		        if (c.updateAll && c.updateAll.length){
+		            for (var i = 0; i < c.updateAll.length; i++){
+		                var item = new db[es].createNew(c.updateAll[i]);
+		                db[es].attach(item);
+		                item.entityState = $data.EntityState.Modified;
+		            }
+		        }
+		    }
+		    
+		    //console.log(JSON.stringify(collections));
+		    db.saveChanges(callback);
+		});
+	    }
+	}
+});
+
+app.listen(3000);
