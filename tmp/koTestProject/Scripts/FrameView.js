e2772c70d2cc077a83fd789fae964f57e1c42756
Peter Nochta <peter.nochta@jaydata.org>
2012-07-27 10:46:51 +0200

    Add testproj

---
+++ tmp/koTestProject/Scripts/FrameView.js
@@ -0,0 +1,8 @@
+﻿$data.Class.define('$JayScrum.FrameView', null, null, {
+    constructor: function (name) {
+
+    },
+    templateName: { dataType: $data.String },
+    loadPath: { dataType: $data.String },
+    templateSource: {dataType: $data.String}
+}, null);
---
+++ tmp/koTestProject/Scripts/_references.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="/Scripts/lib/ko.js"/>
+/// <reference path="/Scripts/lib/datajs-1.0.3.js"/>
+/// <reference path="/Scripts/lib/JayData.js"/>
+/// <reference path="/Scripts/JayDataModuls/knockout.js"/>
+/// <reference path="/Scripts/FrameView.js"/>
+/// <reference path="/Scripts/Frame.js"/>
+/// <reference path="/Scripts/FrameApp.js"/>
+/// <reference path="/Scripts/app.js"/>
