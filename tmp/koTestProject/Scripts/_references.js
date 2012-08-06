5d54cc3390f1bbc2a365a7bca8ec5ec562f2b090
Peter Nochta <peter.nochta@jaydata.org>
2012-08-06 10:50:33 +0200

    remove unused files

--- tmp/koTestProject/Scripts/_references.js
+++
@@ -1,8 +0,0 @@
-﻿/// <reference path="/Scripts/lib/ko.js"/>
-/// <reference path="/scripts/lib/datajs-1.0.3.js"/>
-/// <reference path="/scripts/lib/JayData.js"/>
-/// <reference path="/scripts/jaydatamodules/knockout.js"/>
-/// <reference path="/scripts/FrameView.js"/>
-/// <reference path="/scripts/Frame.js"/>
-/// <reference path="/scripts/FrameApp.js"/>
-/// <reference path="/scripts/app.js"/>
