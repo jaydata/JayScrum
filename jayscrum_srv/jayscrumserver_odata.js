01da9bf49a2df6da1cc72a20a2204b17ce60abac
Peter Aron Zentai <peter.zentai@avangrade.com>
2012-08-09 15:33:17 +0200

    db provisioning implemented

--- jayscrum_srv/jayscrumserver_odata.js
+++ jayscrum_srv/jayscrumserver_odata.js
@@ -1,8 +1,20 @@
 var $data = require('jaydata');
-var ctx = require('./jayscrumcontext.js');
-
+var ctx = require('./jayscrumcontext');
+var appContextType = require('./appdb_context').serviceType;
 var connect = require('connect');
 var app = connect();
+
+console.dir(typeof ad);
+
+function publishDatabaseInstance(urlName, dbName, dbType) {
+    console.log("Publishing database:" + urlName + ">" + dbName + ">" + dbType);
+    var dbContextType = require('./' + dbType).serviceType;
+    console.log("dbContextType:" + dbContextType);
+
+    app.use("/" + urlName, $data.JayService.createAdapter(dbContextType, function() {
+        return new dbContextType({name: 'mongoDB', databaseName: dbName })
+    }));
+}
 
 app.use(function (req, res, next) {
 
@@ -15,18 +27,60 @@ app.use(function (req, res, next) {
         next();
     }
 });
-app.use("/CreateDatabase", function(req, res){
-    console.log('Create database');
-    //res.write('OK');
-    res.end();
 
-});
 app.use(connect.query());
+
 app.use($data.JayService.OData.BatchProcessor.connectBodyReader);
 
 
+app.use("/CreateDatabase", function(req, res){
+    var dbName = req.query.dbName, dbType = req.query.schemaName;
 
-app.use("/", $data.JayService.createAdapter(LightSwitchApplication.ApplicationData, function () {
+
+
+    var appdb = new appContextType({ name:'mongoDB', databaseName:'appdb_00' });
+
+    var result = {
+       _command: 'CreateDatabase',
+        dbName: dbName,
+        dbType: dbType
+    }
+
+    appdb.onReady( function() {
+        appdb.Databases.filter("it.dbName == this.dbName", {dbName: dbName}).toArray( function(items) {
+            if (items.length > 0) {
+                result.status = 'error';
+                result.message = 'duplicate db name';
+                res.end(JSON.stringify(result));
+            };
+
+            appdb.Databases.add({dbName: dbName, dbType: dbType});
+            appdb.saveChanges(function() {
+                publishDatabaseInstance(dbName, dbName, dbType);
+                result.status = 'ok';
+                res.end(JSON.stringify(result));
+            })
+
+        })
+
+
+    });
+});
+
+
+
+var appdb = new appContextType({ name:'mongoDB', databaseName:'appdb_00' });
+
+appdb.onReady(function() {
+    appdb.Databases.forEach( function(database) {
+        console.log("publishing db:" + database.dbName + " of type:"  + database.dbType);
+        publishDatabaseInstance(database.dbName, database.dbName, database.dbType);
+    })
+});
+
+
+
+app.use("/xxx", $data.JayService.createAdapter(LightSwitchApplication.ApplicationData, function () {
     console.log(arguments);
     return new LightSwitchApplication.ApplicationData({ name:'mongoDB', databaseName:'jayScrum' });
 }));
