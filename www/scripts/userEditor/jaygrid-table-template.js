c0edbe190e556bece7cda20e6ac8c4662af36fd4
RynoRn <horvathlaszlo88@gmail.com>
2012-08-29 12:44:24 +0200

    Users manager view has been designed.

--- www/scripts/userEditor/jaygrid-table-template.js
+++ www/scripts/userEditor/jaygrid-table-template.js
@@ -10,40 +10,27 @@
     var templateList = [
         ["jay-data-grid",
             "<form data-bind='submit:save'>\
-            <div data-bind='visible: source' class='action-buttons'>\
-                <span class='iblock'>\
-                    <span data-x-bind='text: $root.discriminatorValue'></span>\
-                    <a class='btn btn-info' href='#' data-bind='click: addNew'><i class='icon-plus icon-white'></i> New</a> \
-                    <a class='btn btn-danger' href='#' data-bind='click:removeAll'><i class='icon-trash icon-white'></i> Remove all</a>\
-                    <input class='btn btn-success' type='submit' value='Save' data-bind='visible: pendingChanges' />\
-                </span>\
-                <span class='iblock pull-right'>\
-                    <span class='title'>Sort:</span><select class='input-medium' data-bind='options: columns, optionsValue: \"name\", optionsText: \"name\", value: sortColumn'></select>\
-                </span>\
+            <div class='ohidden margint15' data-bind='visible: source' class='action-buttons'>\
+                <span data-x-bind='text: $root.discriminatorValue'></span>\
+                <span class='btn btn-info icon-action add fleft' href='#' data-bind='click: addNew'><span></span></span> \
+                <span class='btn btn-danger icon-action delete fleft' href='#' data-bind='click:removeAll'><span></span></span>\
+                <input class='btn btn-success icon-action save fleft' type='submit' value='' data-bind='visible: pendingChanges' />\
             </div>\
-            <table data-bind='visible: source' class='jay-data-grid table table-bordered table-hover' border='0'> \
+            <table data-bind='visible: source' class='jay-data-grid metro-table margin' border='0'> \
                 <!-- ko template: { name: 'jay-data-grid-head' } --> \
                 <!-- /ko -->\
                 <!-- ko template: { name: 'jay-data-grid-body' } --> \
                 <!-- /ko -->\
             </table>\
                 <div data-bind='visible: source' class='action-buttons'>\
-                    <span class='iblock'>\
-                        <a class='btn btn-info' href='#' data-bind='click: addNew'><i class='icon-plus icon-white'></i> New</a> \
-                        <input class='btn btn-success' type='submit' value='Save' data-bind='visible: pendingChanges'/>\
-                    </span>\
-                    <br \>\
-                    <span class='iblock margint10'>\
-                        <select class='input-small' data-bind='options: ko.utils.range(1,50), value: pageSize, visible: pageSize() > 0'></select>\
-                        <span data-bind='visible: pendingChanges, text: pendingStatusInfo()' />\
-                    </span>\
-                    <div class='pagination pagination-centered'>\
-                        <ul>\
-                        <li><a href='#' data-bind='click:goToPreviousPage'>&#171;</a>\</li>\
-                        <li><select class='input-mini' data-bind='options: pages, value: currentPage'></select></li>\
-                        <li><a href='#' data-bind='click:goToNextPage'>&#187;</a></li>\
-                        </ul>\
+                    <div class='ohidden marginb20'>\
+                        <a class='btn btn-info icon-action add fleft' href='#' data-bind='click: addNew'><span></span></a> \
+                        <input class='btn btn-success icon-action save fleft' type='submit' value='' data-bind='visible: pendingChanges'/>\
                     </div>\
+                    <span class='iblock'>\
+                        <select class='input-small iblock fleft metro-select marginr10' data-bind='options: ko.utils.range(1,50), value: pageSize, visible: pageSize() > 0'></select>\
+                        <span class='top5' data-bind='visible: pendingChanges, text: pendingStatusInfo()'></span>\
+                    </span>\
                 </div>\
             </form>"],
 
@@ -75,9 +62,9 @@
         //</span>\
         ["jay-data-grid-control-cell",
             "<td>\
-                <div  data-bind='foreach: itemCommands'>\
-                    <a href='#' class='btn' data-bind='click: execute.bind($data,$parents[1]), \
-                                           visible: visible.call($data,$parents[1]), text: displayName'></a>\
+                <div class='tacenter' data-bind='foreach: itemCommands'>\
+                    <span class='icon-action' data-bind='click: execute.bind($data,$parents[1]), \
+                                           visible: visible.call($data,$parents[1]), attr: { class: \"icon-action \" + displayName.toLowerCase() }'><span></span></span>\
                 </div>\
             </td>"],
 
@@ -88,7 +75,10 @@
             "<span data-bind='text: value'></span>"],
 
         ["jay-data-grid-$data.Boolean-display",
-            '<input type="checkbox" data-bind="checked: value" disabled />'],
+            "<div class='metro-checkbox white fnone_'>\
+                '<input class='disabled' type='checkbox' data-bind='checked: value' disabled />\
+                '<label data-bind='attr: { for: $parent[name] }'></label>'\
+            </div>"],
 
         ["jay-data-grid-$data.String-display",
             '<span data-bind="text: value"></span>'],
@@ -100,16 +90,19 @@
             '<input type="text" data-bind="value: value, attr: { required: metadata.required }" />'],
 
         ["jay-data-grid-$data.Boolean-editor",
-            '<input type="checkbox" data-bind="checked: $parent[name]"  />'],
+            "<div class='metro-checkbox white fnone_'>\
+                '<input type='checkbox' data-bind='checked: $parent[name], attr: { id: $parent[name] }' />\
+                '<label data-bind='attr: { for: $parent[name] }'></label>'\
+            </div>"],
 
         ["jay-data-grid-Edm.String-editor",
             "<input type='text' data-bind='value: value, attr: { required: metadata[\"required\"] }, css: { verror: owner.ValidationErrors }' />" ],
 
         ["jay-data-grid-Edm.Int32-editor",
             "<input type='range' min=1 max=10 \
-data-bind='value: value, attr: { required: $data[\"required\"] }, css: { verror: owner.ValidationErrors }' />" ]
-
+                data-bind='value: value, attr: { required: $data[\"required\"] }, css: { verror: owner.ValidationErrors }' />" ]
     ];
+
     $data.jayGridTemplates = $data.jayGridTemplates || {};
     $data.jayGridTemplates.tableTemplate = templateList;
 })($data);
