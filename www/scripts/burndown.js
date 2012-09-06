97f11284b0cd79076e637197a2208092a67dc3df
Peter Nochta <peter.nochta@jaydata.org>
2012-09-06 19:43:55 +0200

    bugfix

--- www/scripts/burndown.js
+++ www/scripts/burndown.js
@@ -1,19 +1,20 @@
 function DisplayBurndownChart() {
     var rawObject = JayScrum.app.selectedFrame().data().summaryList().SprintBurndownData();
-
+    var xMaxvalue = Math.max.apply(null, rawObject.remainingLine.map(function(item){return item[1]}));
+    xMaxvalue = xMaxvalue + (xMaxvalue*0,1);
     // A couple flotr configuration options:
     var options = {
             shadowSize: 0,
-            colors: ['#ffa500', '#ffa500', '#da0134', '#24A0DA', '#24A0DA'],
+            colors: ['#24A0DA', '#ffa500', '#da0134'],
             xaxis: {
                 min: 0,
-                max: rawObject.length-1,
+                max: (rawObject.length-1)+0.5,
                 tickDecimals: 0,
                 margin: true
             },
             yaxis: {
                 min: 0,
-                max: rawObject.remainingLine[rawObject.remainingLine.length - 1][1],
+                max: xMaxvalue,
                 tickDecimals: 1
             },
             grid: {
